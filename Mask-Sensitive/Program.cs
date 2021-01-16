e1206b541bba2b9c9017aa803ffbbdd9fe0e5a21
Robert Bandstra <rob.bandstra@gmail.com>
2021-01-15 19:17:06 -0600

    program takes user input and masks all but the last 4 characters

---
+++ Mask-Sensitive/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Mask_Sensitive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tell me a secret!");
+            var secret = Console.ReadLine();
+            var maskedSecret = secret.Substring(secret.Length - 4).PadLeft(secret.Length, '*');
+
+            Console.WriteLine(maskedSecret);
+        }
+    }
+}
